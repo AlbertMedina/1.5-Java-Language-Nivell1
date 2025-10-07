d28f9cb7b49e050857bf194fe36f18dbd3eeda86
Albert Medina <albertmecu@gmail.com>
2025-10-07 21:48:39 +0200

    feat: added config.properties and implemented a new Main for level 2 to test it by saving the given directory content tree in the given file

---
+++ src/main/java/level2/Main.java
@@ -0,0 +1,32 @@
+package level2;
+
+import level1.FileUtils;
+
+import java.io.*;
+import java.util.Properties;
+
+public class Main {
+
+    private static final String PROPERTIES_FILE_PATH = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "config.properties";
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream(PROPERTIES_FILE_PATH)) {
+            
+            properties.load(fileInputStream);
+
+            // getting properties
+            String directoryPath = properties.getProperty("DIRECTORY_PATH").replace("/", File.separator);
+            String directoryContentTreeFilePath = properties.getProperty("DIRECTORY_CONTENT_TREE_FILE_PATH").replace("/", File.separator);
+
+            // saving directory content tree
+            String directoryContentTree = FileUtils.getDirectoryContentAlphabetically(directoryPath, true);
+            FileUtils.saveToTxt(directoryContentTree, directoryContentTreeFilePath);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
