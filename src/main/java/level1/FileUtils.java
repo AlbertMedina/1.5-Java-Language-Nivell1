436907d41ebc9c380c6e44e29643753cbac5b404
Albert Medina <albertmecu@gmail.com>
2025-10-07 12:00:53 +0200

    fix: added missing last modification date to every file/folder shown

--- src/main/java/level1/FileUtils.java
+++ src/main/java/level1/FileUtils.java
@@ -2,6 +2,7 @@ package level1;
 
 import java.io.*;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -21,7 +22,7 @@ public class FileUtils {
     private static String getDirectoryContentAlphabetically(File file, boolean showFullTree, int depth) {
         if (file.exists()) {
             StringBuilder sb = new StringBuilder();
-            sb.append("  ".repeat(depth)).append("- ").append(file.getName()).append(" (").append(file.isDirectory() ? "D" : "F").append(")");
+            sb.append("  ".repeat(depth)).append("- ").append(file.getName()).append(" (").append(file.isDirectory() ? "D" : "F").append(") - Last modification: ").append(new Date(file.lastModified()));
             if (file.isDirectory()) {
                 File[] filesArr = file.listFiles();
                 if (filesArr != null) {
@@ -30,7 +31,7 @@ public class FileUtils {
                         if (showFullTree) {
                             sb.append("\n").append(getDirectoryContentAlphabetically(f, true, depth + 1));
                         } else {
-                            sb.append("\n").append("  ".repeat(depth + 1)).append("- ").append(f.getName()).append(" (").append(f.isDirectory() ? "D" : "F").append(")");
+                            sb.append("\n").append("  ".repeat(depth + 1)).append("- ").append(f.getName()).append(" (").append(f.isDirectory() ? "D" : "F").append(")- Last modification: ").append(new Date(f.lastModified()));
                         }
                     }
                 }
