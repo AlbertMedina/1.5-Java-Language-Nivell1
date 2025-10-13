be8d2bf3369d0efc86de38fa1d7ddf109cf0f8de
Albert Medina <albertmecu@gmail.com>
2025-10-13 09:51:35 +0200

    refactor: added DirectoryService to refactor level 1 code

---
+++ src/main/java/level1/DirectoryService.java
@@ -0,0 +1,46 @@
+package level1;
+
+import Utils.FileUtils;
+
+import java.io.File;
+
+public class DirectoryService {
+
+    private static final String DIRECTORY_PATH = "src" + File.separator + "main";
+    private static final String DIRECTORY_CONTENT_TREE_FILE_PATH = "resources" + File.separator + "directory-content.txt";
+    private static final String SERIALIZED_OBJECT_PATH = "resources" + File.separator + "serialized-object.txt";
+
+    public DirectoryService() {
+    }
+
+    public void start() {
+        // printing directory content alphabetically
+        System.out.println(FileUtils.getDirectoryContentAlphabetically(DIRECTORY_PATH, false));
+
+        System.out.println();
+
+        // printing directory content full tree alphabetically
+        System.out.println(FileUtils.getDirectoryContentAlphabetically(DIRECTORY_PATH, true));
+
+        System.out.println();
+
+        // saving directory content tree
+        String directoryContentTree = FileUtils.getDirectoryContentAlphabetically(DIRECTORY_PATH, true);
+        FileUtils.saveToTxt(directoryContentTree, DIRECTORY_CONTENT_TREE_FILE_PATH);
+
+        // getting and printing saved directory content tree
+        System.out.println(FileUtils.readFromTxt(DIRECTORY_CONTENT_TREE_FILE_PATH));
+
+        System.out.println();
+
+        // serializing object
+        MySerializableObject myObj = new MySerializableObject("Hello", 'A', 1);
+        FileUtils.serializeObject(myObj, SERIALIZED_OBJECT_PATH);
+
+        // deserializing object
+        MySerializableObject myDeserializedObj = (MySerializableObject) FileUtils.deserializeObject(SERIALIZED_OBJECT_PATH);
+        if (myDeserializedObj != null) {
+            System.out.println(myDeserializedObj);
+        }
+    }
+}
